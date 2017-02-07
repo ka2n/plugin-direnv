fc020a9cc88f0191f573341d7ffba8abc9e4d695
Katsuma Ito <katsumai@gmail.com>
2017-02-07 10:56:56 +0900

    Make more reactive

--- init.fish
+++ init.fish
@@ -1,6 +1,5 @@
 if type -q direnv
-  eval (direnv export fish)
-  function __direnv_export_eval --on-variable PWD
+  function __direnv_export_eval --on-event fish_prompt
     status --is-command-substitution; and return
     eval (direnv export fish)
   end
